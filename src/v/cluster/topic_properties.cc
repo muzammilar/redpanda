4df09bca48db98e3122342afa1c1831fd0418979
Evgeny Lazin <4lazin@gmail.com>
2025-01-22 14:35:36 -0500

    cluster: Propagate 'allow gaps' property to ntp-config
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cluster/topic_properties.cc
+++ src/v/cluster/topic_properties.cc
@@ -183,6 +183,7 @@ topic_properties::get_ntp_cfg_overrides() const {
     ret.cloud_topic_enabled = cloud_topic_enabled;
     ret.tombstone_retention_ms = delete_retention_ms;
     ret.min_cleanable_dirty_ratio = min_cleanable_dirty_ratio;
+    ret.remote_allow_gaps = remote_topic_allow_gaps;
     return ret;
 }
 
