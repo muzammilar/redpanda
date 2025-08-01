b3a670dbb19ab989ba4b2ba76d2d6531f84ba529
Travis Downs <travis.downs@redpanda.com>
2025-08-01 15:45:12 -0400

    frag vector test: remove unused headers

--- src/v/container/tests/fragmented_vector_test.cc
+++ src/v/container/tests/fragmented_vector_test.cc
@@ -22,9 +22,7 @@
 #include <cstdint>
 #include <initializer_list>
 #include <limits>
-#include <memory>
 #include <numeric>
-#include <span>
 #include <stdexcept>
 #include <type_traits>
 #include <vector>
