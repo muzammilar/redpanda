bfe855aff43d148926596f537f51bf0d4c09ba27
Travis Downs <travis.downs@redpanda.com>
2025-10-28 12:10:55 -0300

    type-check: skip the docker build in GHA
    We already build it using the build-push-action, so no need to do it
    again (and the cache is not effective as different drivers are used).
    
    We also need to align the docker image name in GHA and the type-check.sh
    script, otherwise we won't find it (the latter used "checking" vs
    "check" in GHA).

--- tools/type-checking/type-check.sh
+++ tools/type-checking/type-check.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-image_name=python-type-checking
+image_name=python-type-check
 tag=redpanda-data/$image_name
 
 # Get the directory containing this script
@@ -14,11 +14,14 @@ ROOT_DIR="$(cd "$SCRIPT_DIR/../.." && pwd)"
 TESTS_DIR="$ROOT_DIR/tests"
 TC_DIR="$ROOT_DIR/tools/type-checking"
 
-# Build the Docker image
-echo "Building Docker image $tag..."
-
-docker build -t $tag -f "tools/type-checking/Dockerfile" \
-  ${TARGET:+--target=$TARGET} ${TC_DOCKER_ARGS-} tests
+# Build the Docker image unless skipped
+if [[ ${TC_SKIP_DOCKER_BUILD:-0} != "0" ]]; then
+  echo "Skipping Docker build because TC_SKIP_DOCKER_BUILD=${TC_SKIP_DOCKER_BUILD}" >&2
+else
+  echo "Building Docker image $tag..."
+  docker build -t $tag -f "tools/type-checking/Dockerfile" \
+    ${TARGET:+--target=$TARGET} ${TC_DOCKER_ARGS-} tests
+fi
 
 # Run the container with the tests directory mounted
 # echo "Running type checker in Docker container..."
