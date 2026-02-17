ee19c903fd8648113a2b3dc7a01d89c8fab4b15d
Alexey Bashtanov <alexey.bashtanov@redpanda.com>
2026-02-17 16:59:52 +0000

    c/data-migrate/group-proxy: refactor as PImpl, accept as ss::shared_ptr
    Indirection is needed because ss::sharded cannot create instances of a
    child type. Previously it was an std::unique_ptr, but later we'll need
    to implement stop() member function we need a custom wrapper.
    
    Change data_migrations frontend, router, backend, and worker to accept
    a ss::shared_ptr to group_proxy instance instead of a bare reference to
    impl, in order to hold the instances for as long as necessary even if
    ss::sharded is destroyed.

--- src/v/cluster/data_migration_group_proxy.h
+++ src/v/cluster/data_migration_group_proxy.h
@@ -17,39 +17,90 @@
 #include "model/fundamental.h"
 #include "model/timeout_clock.h"
 
+#include <memory>
+
 namespace cluster::data_migrations {
 
 /*
- * Abstract class for a proxy component to operate kafka layer consumer groups
- * from data migration components.
+ * Abstract class wrapper for a proxy component to operate kafka layer consumer
+ * groups from data migration components.
  */
 class group_proxy {
 public:
-    virtual ~group_proxy() = default;
+    class impl {
+    public:
+        virtual ~impl() = default;
 
-    virtual std::optional<model::partition_id>
-    partition_for(const kafka::group_id& group) = 0;
+        virtual std::optional<model::partition_id>
+        partition_for(const kafka::group_id& group) = 0;
 
-    virtual ss::future<result<model::offset>> set_blocked_for_groups(
-      const model::ntp& co_ntp,
-      const chunked_vector<kafka::group_id>&,
-      bool to_block,
-      model::revision_id revision_id)
-      = 0;
+        virtual ss::future<result<model::offset>> set_blocked_for_groups(
+          const model::ntp& co_ntp,
+          const chunked_vector<kafka::group_id>&,
+          bool to_block,
+          model::revision_id revision_id)
+          = 0;
 
-    virtual ss::future<std::error_code> delete_groups(
+        virtual ss::future<std::error_code> delete_groups(
+          const model::ntp& co_ntp,
+          const chunked_vector<kafka::group_id>& groups,
+          model::revision_id revision_id)
+          = 0;
+
+        virtual ss::future<bool>
+        assure_topic_exists(model::timeout_clock::time_point deadline) = 0;
+
+        virtual ss::future<get_group_offsets_reply>
+        get_group_offsets(get_group_offsets_request&& req) = 0;
+
+        virtual ss::future<set_group_offsets_reply>
+        set_group_offsets(set_group_offsets_request&& req) = 0;
+    };
+
+    explicit group_proxy(std::unique_ptr<impl> impl)
+      : _impl(std::move(impl)) {}
+
+    std::optional<model::partition_id>
+    partition_for(const kafka::group_id& group) {
+        if (_gate.is_closed()) {
+            return std::nullopt;
+        }
+        return _impl->partition_for(group);
+    }
+
+    ss::future<result<model::offset>> set_blocked_for_groups(
       const model::ntp& co_ntp,
       const chunked_vector<kafka::group_id>& groups,
-      model::revision_id revision_id)
-      = 0;
+      bool to_block,
+      model::revision_id revision_id) {
+        return _impl->set_blocked_for_groups(
+          co_ntp, groups, to_block, revision_id);
+    }
 
-    virtual ss::future<bool>
-    assure_topic_exists(model::timeout_clock::time_point deadline) = 0;
+    ss::future<std::error_code> delete_groups(
+      const model::ntp& co_ntp,
+      const chunked_vector<kafka::group_id>& groups,
+      model::revision_id revision_id) {
+        return _impl->delete_groups(co_ntp, groups, revision_id);
+    }
 
-    virtual ss::future<get_group_offsets_reply>
-    get_group_offsets(get_group_offsets_request&& req) = 0;
+    ss::future<bool>
+    assure_topic_exists(model::timeout_clock::time_point deadline) {
+        return _impl->assure_topic_exists(deadline);
+    }
 
-    virtual ss::future<set_group_offsets_reply>
-    set_group_offsets(set_group_offsets_request&& req) = 0;
+    ss::future<get_group_offsets_reply>
+    get_group_offsets(get_group_offsets_request&& req) {
+        return _impl->get_group_offsets(std::move(req));
+    }
+
+    ss::future<set_group_offsets_reply>
+    set_group_offsets(set_group_offsets_request&& req) {
+        return _impl->set_group_offsets(std::move(req));
+    }
+
+private:
+    std::unique_ptr<impl> _impl;
 };
+
 } // namespace cluster::data_migrations
