a64227b4efbbca40d99324061ece8434714735d1
Evgeny Lazin <4lazin@gmail.com>
2026-03-09 11:51:12 -0400

    ct: Fix work accounting in the scheduler
    The write request scheduler is accounting for the x-shard work
    incorrectly. Because of that it often splits groups agressively which
    causes uploaded L0 objects to be smaller than neccessary.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cloud_topics/level_zero/pipeline/write_pipeline.cc
+++ src/v/cloud_topics/level_zero/pipeline/write_pipeline.cc
@@ -273,11 +273,12 @@ void write_pipeline<Clock>::stage::signal_next_stage() {
 template<class Clock>
 void write_pipeline<Clock>::stage::enqueue_foreign_request(
   write_request<Clock>& req, bool signal) {
-    // Foreign requests are proxied from another shard where their bytes
-    // were already accounted for. We place them directly at the next stage
-    // without any byte accounting.
     auto next = _parent->next_stage(_ps);
     req.stage = next;
+    if (next != unassigned_pipeline_stage) {
+        auto idx = static_cast<size_t>(next()->get_numeric_id());
+        _parent->_stage_bytes.at(idx) += req.size_bytes();
+    }
     _parent->get_pending().push_back(req);
     if (signal) {
         _parent->signal(next);
