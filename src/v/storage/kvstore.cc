4027166fc21cad0b953eccc21e315f4865a3e9d5
Willem Kaufmann <willem.kaufmann@redpanda.com>
2026-06-08 17:38:55 -0400

    `kvstore`: set `is_compaction_enabled=false` flag
    Otherwise we reach into the `ntp_config` (with 0 overrides) and try to
    probe the cluster default for `log_cleanup_policy`, which is incorrect
    in the `kvstore` context.

--- src/v/storage/kvstore.cc
+++ src/v/storage/kvstore.cc
@@ -428,7 +428,7 @@ ss::future<> kvstore::recover() {
 
         auto segments = co_await recover_segments(
           partition_path(_ntpc),
-          _ntpc.is_locally_compacted(),
+          /*is_compaction_enabled=*/false,
           [] { return std::nullopt; },
           _as,
           config::shard_local_cfg().storage_read_buffer_size(),
